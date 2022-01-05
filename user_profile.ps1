d7a1b442f4f5e5aa537fe2cf4edd42d3c139ca81
Levy Araujo <levydun@gmail.com>
2022-01-05 19:45:11 -0300

    Powershell config

---
+++ user_profile.ps1
@@ -0,0 +1,40 @@
+Import-Module posh-git
+Import-Module oh-my-posh
+
+# Load Prompt
+function Get-ScriptDirectory { Split-Path $MyInvocation.ScriptName }
+$omp_config = Join-Path $PSScriptRoot ".\levy.omp.json"
+oh-my-posh --init --shell pwsh --config $omp_config | Invoke-Expression
+Set-PoshPrompt -Theme powerlevel10k_rainbow
+
+Import-Module -Name Terminal-Icons
+
+# PSReadLine
+Set-PSReadLineOption -EditMode Emacs
+Set-PSReadLineOption -BellStyle None
+Set-PSReadLineKeyHandler -Chord 'Ctrl+d' -Function DeleteChar
+Set-PSReadLineOption -PredictionSource History
+Set-PSReadLineOption -PredictionViewStyle ListView
+
+# Fzf
+Import-Module PSFzf
+Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+f' -PSReadlineChordReverseHistory 'Ctrl+r'
+
+# Env
+$env:GIT_SSH = "C:\Windows\system32\OpenSSH\ssh.exe"
+
+# Alias
+Set-Alias -Name vim -Value nvim
+Set-Alias g git
+Set-Alias grep findstr
+Set-Alias tig 'C:\Program Files\Git\usr\bin\tig.exe'
+Set-Alias less 'C:\Program Files\Git\usr\bin\less.exe'
+Set-Alias python2 'C:\Python27\python.exe'
+
+# Utilities
+function which ($command) {
+  Get-Command -Name $command -ErrorAction SilentlyContinue |
+    Select-Object -ExpandProperty Path -ErrorAction SilentlyContinue
+}
+
+cls
